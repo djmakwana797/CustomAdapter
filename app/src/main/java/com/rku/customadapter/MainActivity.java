4cd2e1925efb917b3521e30427b53a805d6fce9c
djmakwana797 <dmakwana979@rku.ac.in>
2020-09-23 22:48:58 +0530

    Added ListView with Custom Adapter.

--- app/src/main/java/com/rku/customadapter/MainActivity.java
+++ app/src/main/java/com/rku/customadapter/MainActivity.java
@@ -3,12 +3,32 @@ package com.rku.customadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lstData;
+    ArrayList<Item> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lstData = findViewById(R.id.lstData);
+        list = generateData();
+        MyAdapter adapter = new MyAdapter(this,list);
+        lstData.setAdapter(adapter);
+    }
+
+    private ArrayList<Item> generateData(){
+        ArrayList<Item> list = new ArrayList<Item>();
+        list.add(new Item("One","First item on the list",R.drawable.one));
+        list.add(new Item("Two","Second item on the list",R.drawable.two));
+        list.add(new Item("Three","Third item on the list",R.drawable.three));
+
+        return list;
     }
 }
